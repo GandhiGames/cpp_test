46d8ac167bff855a8d69a9a5ab1618d6b6412c94
Robert Wells <robert.wells@gandhigames.co.uk>
2016-02-21 16:04:37 +0000

    initial project structure

---
+++ cpp_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_test
+//
+//  Created by Robert Wells on 21/02/2016.
+//  Copyright © 2016 Robert Wells. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
