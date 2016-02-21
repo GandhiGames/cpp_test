4b5113f9579348708b70834024e428c4624a3bf4
Robert Wells <robert.wells@gandhigames.co.uk>
2016-02-21 16:44:41 +0000

    test ascii table print and psuedorand num generator

--- cpp_test/main.cpp
+++ cpp_test/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int getRandomNumber(int start, int end) {
+    srand(static_cast<unsigned>(time(0)));
+    return (rand() % end) + start;
+}
+
+void printAsciiTable() {
+    char temp;
+     
+     for (int i = 0; i < 128; i++) {
+     temp = i;
+     cout<<i<<": "<<temp<<endl;
+     }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    printAsciiTable();
+    
+  
+    cout<<getRandomNumber(1, 10)<<endl;
+    
+    
     return 0;
 }
